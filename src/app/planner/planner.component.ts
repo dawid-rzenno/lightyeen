aa2bd7e9515c483710a4d8d2b6bd4197fdc9f25f
Dawid <dawid.rzenno@protonmail.com>
2022-12-03 16:04:02 +0100

    Generate Planner feature module and component

---
+++ src/app/planner/planner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ly-planner',
+  templateUrl: './planner.component.html',
+  styleUrls: ['./planner.component.scss']
+})
+export class PlannerComponent {
+
+}
---
+++ src/app/planner/planner.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlannerComponent } from './planner.component';
+
+
+
+@NgModule({
+  declarations: [
+    PlannerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PlannerModule { }

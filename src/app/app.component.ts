357af033ed544398e92f79256fe78d5de7617bf3
Dawid <dawid.rzenno@protonmail.com>
2022-12-03 16:01:52 +0100

    Use 'ly' prefix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ly-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lightyeen';
 }
